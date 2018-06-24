40ddf1d8e2ed628d95a86c8349d430009a46bc52
Younes DECHRAOUI <ydechraoui@gmail.com>
2018-06-24 18:33:42 +0100

    first commit

---
+++ src/main/java/io/devYounes/contactSoft/ContactSoftApplication.java
@@ -0,0 +1,12 @@
+package io.devYounes.contactSoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactSoftApplication.class, args);
+	}
+}
---
+++ src/test/java/io/devYounes/contactSoft/ContactSoftApplicationTests.java
@@ -0,0 +1,16 @@
+package io.devYounes.contactSoft;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactSoftApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
